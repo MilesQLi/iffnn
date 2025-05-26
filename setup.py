73a340c352dd0beb09d0adc9632f25888cdcf361
miles <your_email@example.com>
2025-05-25 21:08:44 -0400

    update

--- setup.py
+++ setup.py
@@ -3,24 +3,36 @@ from setuptools import setup, find_packages
 setup(
     name='iffnn',
     version='0.2.0',
-    author='Miles Q. Li', 
-    author_email='miles.qi.li@mail.mcgill.ca',    # Replace
+    author='Miles Q. Li',
+    author_email='miles.qi.li@mail.mcgill.ca',
     description='Interpretable Feedforward Neural Network Library based on Li et al.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/MilesQLi/iffnn', # Replace with your repo URL
-    packages=find_packages(),
+    url='https://github.com/MilesQLi/iffnn',
+    project_urls={
+        'Documentation': 'https://github.com/MilesQLi/iffnn#readme',
+        'Source': 'https://github.com/MilesQLi/iffnn',
+        'Bug Tracker': 'https://github.com/MilesQLi/iffnn/issues',
+    },
+    packages=find_packages(exclude=['tests*', 'examples*']),
     install_requires=[
-        'torch>=1.7.0', # Specify a reasonable minimum version
+        'torch>=1.7.0',
         'tqdm>=4.50.0',
-        # scikit-learn is only needed for the example, not the core library
     ],
+    extras_require={
+        'dev': ['pytest', 'flake8'],
+        'examples': ['scikit-learn>=0.24.0'],
+    },
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License', # Choose an appropriate license
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires='>=3.7', # Specify minimum Python version
+    keywords='interpretable neural networks explainability PyTorch AI',
+    python_requires='>=3.7',
 )
